94ba709ac1d2e75c7452b28c924308d7d91b59a0
Romulo Evangelista de Oliveira <romulop40@gmail.com>
2023-03-25 02:39:22 -0300

    Calculadora em Java

---
+++ src/javaapplicationminhacalculadora/JavaApplicationMinhaCalculadora.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplicationminhacalculadora;
+
+import Apresentacao.JFrameCalculadora;
+
+/**
+ *
+ * @author home
+ */
+public class JavaApplicationMinhaCalculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrameCalculadora jFrameCalculadora = new JFrameCalculadora();
+        jFrameCalculadora.setLocationRelativeTo(null);
+        jFrameCalculadora.setVisible(true);
+    }
+}
